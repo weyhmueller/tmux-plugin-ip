6eb5f76f117c642dff3a83760e7597493c16a7e3
Oliver Weyhmueller <oliver@weyhmueller.de>
2020-03-03 08:33:02 +0100

    Update ip.sh

--- scripts/ip.sh
+++ scripts/ip.sh
@@ -7,26 +7,46 @@ source "$CURRENT_DIR/helpers.sh"
 # please note: this plugin is adjusted for my laptop only right now (thinkpad t470s, Ubuntu 17.10)
 # it may be improved in the future
 
-function get_ip_wlan() {
-  no_of_broadcasts="$(ifconfig | grep broadcast | wc -l)"
-  if [ $no_of_broadcasts -gt "0" ]; then
-    ip="$(ifconfig | grep broadcast | awk '{print $2}' | tail -n1)"
+function get_os() {
+  OS=$(uname -s)
+}
+
+function get_ip_wlan_Linux() {
+  ip="$(ifconfig | grep wlp3s0 -A1 | awk '{print $2}' | tail -n1)"
+  if [[ $ip != *"."* ]]; then
+    ip=""
   fi
 }
 
-function get_ip_eth() {
-  ip="$(ifconfig | grep ether | awk '{print $2}')"
+function get_ip_eth_Linux() {
+  ip="$(ifconfig | grep enp -A1 | awk '{print $2}' | tail -n1)"
+  if [[ $ip != *"."* ]]; then
+    ip=""
+  fi
+}
+
+
+function get_ip_wlan_Darwin() {
+  ip="$(ifconfig en0 | grep "inet " | awk '{print $2}' | tail -n1)"
+  if [[ $ip != *"."* ]]; then
+    ip=""
+  fi
+}
+
+function get_ip_eth_Darwin() {
+  ip="$(ifconfig | grep "inet " | awk '{print $2}' | tail -n1)"
   if [[ $ip != *"."* ]]; then
     ip=""
   fi
 }
 
 function print_ip() {
-  get_ip_wlan
+  get_os
+  get_ip_wlan_${OS}
   if [ ! -z "$ip" ]; then
     echo "$ip"
   else
-    get_ip_eth
+    get_ip_eth_${OS}
     if [ ! -z "$ip" ]; then
       echo $ip
     else
